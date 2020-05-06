95c4d3a3b4445588306138ee790d3a96f6b59c84
Nils Niemann <nils.niemann@dfki.de>
2020-05-06 17:07:11 +0200

    added helper to construct simplified network representation from rete

---
+++ src/ReteVisualSerialization.cpp
@@ -0,0 +1,183 @@
+#include "ReteVisualSerialization.hpp"
+
+#include <rete-core/AlphaNode.hpp>
+#include <rete-core/AlphaMemory.hpp>
+#include <rete-core/BetaNode.hpp>
+#include <rete-core/BetaMemory.hpp>
+#include <rete-core/BetaBetaNode.hpp>
+#include <rete-core/ProductionNode.hpp>
+
+namespace sempr { namespace gui {
+
+bool Node::operator<(const Node& other) const
+{
+    return id < other.id;
+}
+
+bool Edge::operator<(const Edge& other) const
+{
+    if (from == other.from) return to < other.to;
+    return from < other.from;
+}
+
+
+void CreateVisualGraphVisitor::addNode(
+        const std::string& id,
+        const std::string& label)
+{
+    graph_.nodes.insert({id, label});
+}
+
+void CreateVisualGraphVisitor::addEdge(
+        const std::string& from,
+        const std::string& to)
+{
+    graph_.edges.insert({from, to});
+}
+
+Graph CreateVisualGraphVisitor::graph() const
+{
+    return graph_;
+}
+
+void CreateVisualGraphVisitor::visit(rete::AlphaNode* node)
+{
+    std::string id = node->getDOTId();
+
+    // stop if already visited
+    if (visited_.find(id) != visited_.end()) return;
+    visited_.insert(id);
+
+    std::string label = static_cast<rete::Node*>(node)->getDOTAttr();
+
+    // add this node
+    addNode(id, label);
+
+    // add edges to the children and visit them
+    std::vector<rete::AlphaNode::Ptr> children;
+    node->getChildren(children);
+
+    for (auto child : children)
+    {
+        if (child)
+        {
+            addEdge(id, child->getDOTId());
+            static_cast<rete::Node*>(child.get())->accept(*this);
+        }
+    }
+
+    // add edge to alpha memory and visit it
+    if (node->getAlphaMemory())
+    {
+        addEdge(id, node->getAlphaMemory()->getDOTId());
+        static_cast<rete::Node*>(node->getAlphaMemory().get())->accept(*this);
+    }
+}
+
+
+void CreateVisualGraphVisitor::visit(rete::AlphaMemory* node)
+{
+    std::string id = node->getDOTId();
+
+    if (visited_.find(id) != visited_.end()) return;
+    visited_.insert(id);
+
+    std::string label = static_cast<rete::Node*>(node)->getDOTAttr();
+
+    // add this node
+    addNode(id, label);
+
+    // visit children
+    std::vector<rete::BetaNode::Ptr> children;
+    node->getChildren(children);
+
+    for (auto child : children)
+    {
+        if (child)
+        {
+            addEdge(id, child->getDOTId());
+            static_cast<rete::Node*>(child.get())->accept(*this);
+        }
+    }
+}
+
+void CreateVisualGraphVisitor::visit(rete::BetaNode* node)
+{
+    std::string id = node->getDOTId();
+
+    if (visited_.find(id) != visited_.end()) return;
+    visited_.insert(id);
+
+    std::string label = static_cast<rete::Node*>(node)->getDOTAttr();
+
+    // add this node
+    addNode(id, label);
+
+    // visit child node
+    if (node->getBetaMemory())
+    {
+        addEdge(id, node->getBetaMemory()->getDOTId());
+        static_cast<rete::Node*>(node->getBetaMemory().get())->accept(*this);
+    }
+}
+
+void CreateVisualGraphVisitor::visit(rete::BetaMemory* node)
+{
+    std::string id = node->getDOTId();
+
+    if (visited_.find(id) != visited_.end()) return;
+    visited_.insert(id);
+
+    std::string label = static_cast<rete::Node*>(node)->getDOTAttr();
+
+    // add this node
+    addNode(id, label);
+
+    // visit beta node children
+    std::vector<rete::BetaNode::Ptr> betaChildren;
+    node->getChildren(betaChildren);
+
+    for (auto child : betaChildren)
+    {
+        if (child)
+        {
+            addEdge(id, child->getDOTId());
+            static_cast<rete::Node*>(child.get())->accept(*this);
+        }
+    }
+
+    // visit production node children
+    std::vector<rete::ProductionNode::Ptr> productionChildren;
+    node->getProductions(productionChildren);
+
+    for (auto child : productionChildren)
+    {
+        if (child)
+        {
+            addEdge(id, child->getDOTId());
+            static_cast<rete::Node*>(child.get())->accept(*this);
+        }
+    }
+}
+
+
+void CreateVisualGraphVisitor::visit(rete::BetaBetaNode* node)
+{
+    visit(static_cast<rete::BetaNode*>(node));
+}
+
+void CreateVisualGraphVisitor::visit(rete::ProductionNode* node)
+{
+    std::string id = node->getDOTId();
+
+    if (visited_.find(id) != visited_.end()) return;
+    visited_.insert(id);
+
+    std::string label = node->getName();
+
+    // add this node
+    addNode(id, label);
+}
+
+
+}}
---
+++ src/ReteVisualSerialization.hpp
@@ -0,0 +1,87 @@
+#ifndef SEMPR_GUI_RETEVISUALSERIALIZATION_HPP_
+#define SEMPR_GUI_RETEVISUALSERIALIZATION_HPP_
+
+#include <cereal/cereal.hpp>
+#include <cereal/types/set.hpp>
+
+#include <rete-core/NodeVisitor.hpp>
+
+#include <string>
+#include <set>
+
+/*
+    This file provides a few classes to help with creating and transmitting a
+    visual representation of a rete network. Some helper structs for the basic
+    representation, which can be created through the use of a node visitor
+    which traverses the graph. These can be serialized to send them over the
+    network.
+*/
+
+namespace sempr { namespace gui {
+
+struct Node {
+    std::string id;
+    std::string label;
+
+    bool operator < (const Node& other) const;
+
+    template <class Archive>
+    void serialize(Archive& ar)
+    {
+        ar( cereal::make_nvp<Archive>("id", id),
+            cereal::make_nvp<Archive>("label", label) );
+    }
+};
+
+struct Edge {
+    std::string from;
+    std::string to;
+
+    bool operator < (const Edge& other) const;
+
+    template <class Archive>
+    void serialize(Archive& ar)
+    {
+        ar( cereal::make_nvp<Archive>("from", from),
+            cereal::make_nvp<Archive>("to", to) );
+    }
+};
+
+struct Graph {
+    std::set<Node> nodes;
+    std::set<Edge> edges;
+
+    template <class Archive>
+    void serialize(Archive& ar)
+    {
+        ar( cereal::make_nvp<Archive>("nodes", nodes),
+            cereal::make_nvp<Archive>("edges", edges) );
+    }
+};
+
+
+class CreateVisualGraphVisitor : public rete::NodeVisitor {
+    Graph graph_;
+
+    // a set of already visited nodes, to not visit them twice
+    std::set<std::string> visited_;
+
+    void addNode(const std::string& id, const std::string& label);
+    void addEdge(const std::string& from, const std::string& to);
+public:
+    void visit(rete::AlphaNode*) override;
+    void visit(rete::AlphaMemory*) override;
+    void visit(rete::BetaNode*) override;
+    void visit(rete::BetaMemory*) override;
+    void visit(rete::BetaBetaNode*) override;
+    void visit(rete::ProductionNode*) override;
+
+    Graph graph() const;
+};
+
+
+
+}}
+
+
+#endif /* include guard: SEMPR_GUI_RETEVISUALSERIALIZATION_HPP_ */
